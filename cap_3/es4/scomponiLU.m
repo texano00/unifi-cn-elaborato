9c9abcf2311146e510ac44a17b425fabb1b494bb
Yuri <yuribacciarini@gmail.com>
2018-02-26 13:01:48 +0100

    do cap_3/es4 and es5

---
+++ cap_3/es4/scomponiLU.m
@@ -0,0 +1,21 @@
+function [L,U] = scomponiLU(LU);
+    n = size(LU,1);
+    L = zeros(n);
+    U = zeros(n);
+    
+    for i=1:n
+        for j=1:n
+            if(j<i)
+                L(i,j) = LU(i,j);
+            end
+            
+            if(j==i)
+                L(i,j) = 1;
+            end
+            
+            if(j>=i)
+                U(i,j) = LU(i,j);
+            end
+        end
+    end
+end
