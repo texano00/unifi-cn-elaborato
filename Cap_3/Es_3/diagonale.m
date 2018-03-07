f8e9e7affbf5e8907f97b4bf90492d4ecff03b18
Zorango95 <freewilly1995@gmail.com>
2018-03-07 21:19:16 +0100

    Leggera descrizione dell'es.3 ancora da terminare

--- Cap_3/Es_3/diagonale.m
+++ Cap_3/Es_3/diagonale.m
@@ -1,24 +1,26 @@
-function x = diagonale(A,x);
+% diagonale(A,x)
+% Metodo per il cacolo del vettore incognite di una matrice diagonale.
+%
+% Input:
+% A: matrice.
+% b: vettore dei termini noti.
+
+function x = diagonale(A,b)
+    x = b;
     if ~ismatrix(A)
         error('The input must be a matrix');
     end
-    
     [n,m] = size(A);
-    
     if(n~=m)
         error('The input must be a square matrix');
     end
-
     if(~isvector(x))
         error('The second parameter must be a vector');
-    end
-    
+    end    
     vectorSize = size(x,1);
-
     if(vectorSize~=n)
         error('The vector must have %i rows, %i passed', n, vectorSize);
-    end
-    
+    end    
     for j=1:n
         x(j) = x(j)/A(j,j);
     end
--- Cap_3/Es_3/risolutoreLDLt.m
+++ Cap_3/Es_3/risolutoreLDLt.m
@@ -1,4 +1,11 @@
-function x = risolutoreLDLt(LDLt, b);
+% risolutoreLDLt(LDLt,b)
+% Metodo per il calcolo di una matrice LDLt.
+
+% Input:
+% LDLt: matrice;
+% b: vettore dei termini noti.
+
+function x = risolutoreLDLt(LDLt, b)
     [L,D,Lt] = scomponiLDLt(LDLt);
     x1 = triangolareInferiore(L,b);
     x2 = diagonale(D,x1);
