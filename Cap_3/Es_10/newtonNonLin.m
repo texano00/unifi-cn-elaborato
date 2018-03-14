9e9738edc3ce630bed5a3eb0f4b32ab8c3763851
Zorango95 <freewilly1995@gmail.com>
2018-03-14 18:59:58 +0100

    Soluzione es.11 da finire

--- Cap_3/Es_10/newtonNonLin.m
+++ Cap_3/Es_10/newtonNonLin.m
@@ -10,10 +10,13 @@
 
 function x = newtonNonLin(f, J, x, imax, tolx)
     i=0;
+    xold=0;
     while(i<imax) && (norm(x-xold)>tolx)
         i=i+1;
         xold=x;
         [J, p] = fattorizzazioneLUpiv(J);
+        J, p
         x = x + risolutoreLUpiv(J, p, -feval(f,x));
+        x, i
     end
 end
