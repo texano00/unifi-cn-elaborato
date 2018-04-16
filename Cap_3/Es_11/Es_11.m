0a0ba3ed167b078b1dc362a36ea2344a6068a470
Zorango95 <freewilly1995@gmail.com>
2018-04-17 01:09:06 +0200

    Spero ultimo ripasso di grafica generale

--- Cap_3/Es_11/Es_11.m
+++ Cap_3/Es_11/Es_11.m
@@ -3,16 +3,14 @@ format short;
 x = [1/2;1/2];
 f = inline('x(1)^2+x(2)^3-x(1)*x(2)');
 F = inline('[2*x(1)-x(2), 3*x(2)^2-x(1)]');
-J=[2-x(2), 2*x(1)-1; 3*x(2)^2-1, 6*x(2)-x(1)];
+J = [2-x(2), 2*x(1)-1; 3*x(2)^2-1, 6*x(2)-x(1)];
 
 tolx = 10^-3;
-
-y1 = newtonNonLin(F, J, x, 100, tolx);
-n1 = norm(y1);
-y1, n1
+x1 = newtonNonLin(F, J, x, 100, tolx);
+n1 = norm(x1);
+x1, n1
 
 tolx = 10^-6;
-
-y2 = newtonNonLin(F, J, x, 100, tolx);
-n2 = norm(y2);
-y2, n2
+x2 = newtonNonLin(F, J, x, 100, tolx);
+n2 = norm(x2);
+x2, n2
