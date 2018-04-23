a624d90a5b43c2af33a919695bbd9151c176df11
Zorango95 <freewilly1995@gmail.com>
2018-04-23 13:24:39 +0200

    Complete es.1-2 cap.5

---
+++ Cap_5/Es_1/trapcomp.m
@@ -0,0 +1,22 @@
+% If = trapcomp(n, a, b, fun)
+%   Formula dei trapezi composita per l'approssimazione dell'integrale
+%   definito di una funzione.
+%
+% Input:
+%   -n: numero di sottointervalli sui quali applicare la formula dei
+%       trapezi semplice.
+%   -a: estremo sinistro dell'intervallo di integrazione;
+%   -b: estremo destro dell'intervallo di integrazione;
+%   -fun: la funzione di cui si vuol calcolare l'integrale;
+%
+% Output:
+%   -If: l'approssimazione dell'integrale definito della funzione.
+
+function [If] = trapcomp(n, a, b, fun)
+    h = (b-a)/n;
+    If = 0;
+    for i = 1:n-1
+        If = If+fun(a+i*h);
+    end
+    If = (h/2)*(2*If + fun(a) + fun(b));
+end
---
+++ Cap_5/Es_2/simpcomp.m
@@ -0,0 +1,22 @@
+% If = simpcomp(n, a, b, fun)
+%   Formula di Simpson composita per l'approssimazione dell'integrale
+%   definito di una funzione.
+%
+% Input:
+%   -n: numero, pari, di sottointervalli sui quali applicare la formula di
+%       Simpson semplice.
+%   -a: estremo sinistro dell'intervallo di integrazione;
+%   -b: estremo destro dell'intervallo di integrazione;
+%   -fun: la funzione di cui si vuol calcolare l'integrale;
+%
+% Output:
+%   -If: l'approssimazione dell'integrale definito della funzione.
+
+function [If] = simpcomp(n, a, b, fun)
+    h = (b-a)/n;
+    If = fun(a)-fun(b);
+    for i=1:n/2
+        If = If + 4*fun(a+(2*i-1)*h)+2*fun((a+2*i*h));
+    end
+    If = If*(h/3);
+end
