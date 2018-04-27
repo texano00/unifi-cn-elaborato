b569de77d691d86d92a2c4ce54b6827d50b10c87
Zorango95 <freewilly1995@gmail.com>
2018-04-27 04:13:45 +0200

    Aggiustato es.10-11-7 Cap.3 e quasi completato es.10 cap.4

--- Cap_3/Es_7/Es_7.m
+++ Cap_3/Es_7/Es_7.m
@@ -1,2 +1,12 @@
-x = triangolareInferioreMod(5,[1;2;3;4;5]);
-x
+b1 = [1; 101*ones(12,1)];
+b2 = 0.1*[1; 101*ones(12,1)];
+x1 = triangolareInferioreMod(100,b1);
+x2 = triangolareInferioreMod(100,b2);
+x1
+x2
+
+A = eye(12)+100*[ zeros(1, 12); eye(11) zeros(1, 11)'];
+k = cond(A);
+ninf = norm(A,inf);
+n1 = norm(A,inf);
+ninv = norm(A^-1, inf);
