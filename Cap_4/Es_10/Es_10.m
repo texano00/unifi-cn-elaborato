f813e36ca29595905fadf963843a7fc0358092c1
Zorango95 <freewilly1995@gmail.com>
2018-04-27 15:47:29 +0200

    Complete es.10 cap.4

---
+++ Cap_4/Es_10/Es_10.m
@@ -0,0 +1,16 @@
+A = [ ones(10,3)];
+j = 2;
+for i = 3:2:length(A)-1
+    A(i,2) =  j;
+    A(i+1,2) = j;
+    A(i,3) = j^2;
+    A(i+1,3) = j^2;
+    j = j+1;
+end
+b = [2.9; 3.1; 6.9; 7.1; 12.9; 13.1; 20.9; 21.1; 30.9; 31.1];
+
+x = risolutoreQR(A,b);
+r = A*x-b;
+n = norm(r)^2;
+format long e
+x, r, n
