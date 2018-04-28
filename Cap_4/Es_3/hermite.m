a17a045fd61bfbfb46a70eab2279e3187f968845
Yuri <yuribacciarini@gmail.com>
2018-04-28 10:18:12 +0200

    comment cap_4/Es_4

--- Cap_4/Es_3/hermite.m
+++ Cap_4/Es_3/hermite.m
@@ -25,6 +25,7 @@ function [y] = hermite(xi, fi, f1i, x)
     fh(1:2:2*n+1) = fi;
     fh(2:2:2*n+2) = f1i;
     nh = length(xh)-1;
+    % Calcolo delle differenze divise
     for i = nh:-2:3
         fh(i) = (fh(i)-fh(i-2))/(xh(i)-xh(i-1));
     end
@@ -33,6 +34,7 @@ function [y] = hermite(xi, fi, f1i, x)
             fh(j) = (fh(j)-fh(j-1))/(xh(j)-xh(j-i));
         end
     end
+    % Horner
     y = fh(nh+1)*ones(size(x));
     for i = nh:-1:1
         y = y.*(x-xh(i))+fh(i);
