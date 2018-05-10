effac196f34836fc209d2cbccc2143499a8f5e97
Zorango95 <freewilly1995@gmail.com>
2018-05-10 16:14:40 +0200

    Refresh es.4 cap.4

--- Cap_4/Es_4/Es_4.m
+++ Cap_4/Es_4/Es_4.m
@@ -1,23 +1,28 @@
 % Soluzione Cap_4 Es_4.
+%
+% -xi: valori ascisse di interpolazione
+% -fi: valori della funzione sin() sui punti di interpolazione xi;
+% -fli: valori derivata della funzione sull ascisse di interpolazione;
+% -x: serie di punti;
+% -y: valori della funzione sin() calcolati sui punti x;
+% -y1: valori del polinomio di lagrange calcolati sui punti x;
+% -y2: valori del polinomio di newton calcolati sui punti x;
+% -y3: valori del polinomio di hermite calcolati sui punti x.
 
 xi = zeros(3,1);
-fi = zeros(3,1);
 f1i = zeros(3,1);
 for i = 0:length(xi)-1
     xi(i+1) = i*pi;
-    fi(i+1) = sin(xi(i+1));
     f1i(i+1) = cos(xi(i+1));
 end
-x = [xi(1); 1; xi(2); 5; xi(3)];
 
+fi = [0;0;0];
+x = (0:0.1:2*pi);
+
+y = sin(x);
 y1 = lagrange(xi, fi, x);
-figure;
-plot(xi, fi, x, y1);
-
 y2 = newton(xi, fi, x);
-figure;
-plot(xi, fi, x, y2);
-
 y3 = hermite(xi, fi, f1i, x);
-figure;
-plot(xi, fi, x, y3);
+
+plot(x,y,x,y1,x,y2,x,y3);
+legend('f','l','n','h')
