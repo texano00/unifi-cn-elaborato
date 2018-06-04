aa49d1d1284ccc91be3984c0804d598bde10529f
Zorango95 <mattia.dautilia@stud.unifi.it>
2018-06-04 12:37:14 +0200

    Refresh code Es_9 ... Completato ... mancano i plot e aggiornare il pdf

--- Cap_4/Es_9/Es_9.m
+++ Cap_4/Es_9/Es_9.m
@@ -3,12 +3,12 @@
 % -f: funzione di Runge;
 % -a: punto estremo destro intervallo;
 % -b: punto estremo sinistro interballo;
-% -n: numero di valutazioni;
+% -n: numero di ascisse di interpolazione.
 
 f = @(x) 1 ./ (1 + 25.*x.^2);
 a = -6;
 b = 6;
-n = 2:2:40;
+n = 2:2:12;
 
 % -x: valori nei quali mi interessa sapere il valore del polimonio interpolante
 x = linspace(a,b);
@@ -27,7 +27,6 @@ for i = 1:length(n)
   plot(x,y)
   hold on
   
-  norm(f(x) - y)
 end
 legend('2','4','6','8','10','12','14','16','18','20','22','24','26','28','30','32','34','36','38','40')
 hold off
